8e4b51e167b3c65555073bbf22498205d7b7cda3
Caleb King <caleb.king536@gmail.com>
2020-07-01 09:48:12 -0600

    initial commit, boilerplate Express App

---
+++ test/app.spec.js
@@ -0,0 +1,9 @@
+/* global expect, supertest */
+const app = require('../src/app');
+
+describe('App', () => {
+  it('GET / responds with 200 containing "Hello, world!"', () =>
+    supertest(app)
+      .get('/')
+      .expect(200, 'Hello, world!'));
+});
